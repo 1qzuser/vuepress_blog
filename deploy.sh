c267c33eb09d36656dd243a73a08dfba35a32a44
程序员峰峰 <1420688044@qq.com>
2022-03-30 16:51:15 +0800

    生成打包文件

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,29 @@
 #!/usr/bin/env sh
 
 # 确保脚本抛出遇到的错误
-set -e
+# set -e
 
 # 生成静态文件
-yarn build
+# yarn build
 
 # 添加到仓库 
-git add .
-git commit -m 'deploy'
+# git add .
+# git commit -m 'deploy'
 
 # 发布到 gitee 仓库
 
-git push -u origin "master"
+# git push -u origin "master"
+
+
+
+set - e
+
+yarn build
+
+cd docd/.vuepress/dist
+
+git add .
+
+git commit -m '生成打包文件'
+
+git push -u origin "dev"
