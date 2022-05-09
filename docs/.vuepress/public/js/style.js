702fecddb61890de65db90a05d863b0f24626863
高峰 <email@sample.com>
2022-05-09 17:51:11 +0800

    修改控制台的样式

--- docs/.vuepress/public/js/style.js
+++ docs/.vuepress/public/js/style.js
@@ -1,5 +1,5 @@
 
 window.onload = (e) =>{
-    console.log("我是程序員峰峰")
+    console.log("%c小伙子，你在找啥呢？联系QQ：274566674", "color:#fff; background: linear-gradient(270deg, #986fee, #8695e6, #68b7dd, #18d7d3); padding: 8px 15px; border-radius: 0 15px 0 15px");
 }
 
