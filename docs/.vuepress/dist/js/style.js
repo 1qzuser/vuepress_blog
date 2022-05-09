9bf180dc890596c56fab780f8efb11ca3930ccaf
高峰 <email@sample.com>
2022-05-09 09:26:16 +0800

    删除编译之后的代码

--- docs/.vuepress/dist/js/style.js
+++
@@ -1,6 +0,0 @@
-
-window.onload = (e) =>{
-    console.log("%c页面加载耗时：29ms | Theme By Joe", "color:#fff; background: linear-gradient(270deg, #986fee, #8695e6, #68b7dd, #18d7d3); padding: 8px 15px; border-radius: 0 15px 0 15px");
-    console.log('\n' + ' %c APlayer v' + APLAYER_VERSION + ' ' + GIT_HASH + ' %c http://aplayer.js.org ' + '\n', 'color: #fadfa3; background: #030307; padding:5px 0;', 'background: #fadfa3; padding:5px 0;');
-}
-
