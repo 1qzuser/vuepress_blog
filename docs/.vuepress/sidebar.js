26eb2dd3ea1e667ef87f1a88adcda95b2cdfe7e8
ffbk <1420688044@qq.com>
2022-09-01 15:15:11 +0800

    增加Java

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -1,53 +1,30 @@
 module.exports = {
-    '/guide/': [
-        {
-            title: '懵逼指南',
-            collapsable: false,
-            children: [
-                { title: '前端学习', path: '/guide/end' },
-                { title: '后端学习', path: '/guide/back' },
-            ]
-        }
-    ],
+    // '/baodian/front/': [
+    //     {
+    //         title: '懵逼指南',
+    //         collapsable: true,
+    //         children: [
+    //             { title: '前端学习', path: '/baodian/front/css' },
+    //             { title: '后端学习', path: '/guide/back' },
+    //         ]
+    //     }
+    // ],
     '/baodian/': [
-        // {
-        //     title: '面试宝典11123',
-        //     collapsable: true,
-        //     children: [
-        //         { text: "前端", link: "/css" },
-        //         { text: "html", link: "/baodian/front/html/" },
-        //         { text: "javascript", link: "/baodian/front/javascript/" },
-        //         { text: "vue", link: "/baodian/front/vue/" },
-        //     ]
-        // }
         {
-            title: '分组2 后端',
-            collapsable: true,
+            title: '前端面试',
             children: [
-              '/baodian/front/html/',
+                { title: 'css', path: 'front/css' },
+                { title: 'html', path: 'front/html' },
+                { title: 'javascript', path: 'front/javascript' },
+                { title: 'vue', path: 'front/vue' },
             ]
-          }
-    
-    ],
-    '/life/': [
+        },
         {
-            title: '记录生活',
-            collapsable: true,
+            title: '后端面试',
             children: [
-                { title: '学习', path: '/life/study' },
-                { title: '生活', path: '/life/lifes' },
+                { title: 'php', path: 'back/php' },
+                { title: 'java', path: 'back/java' },
             ]
-        }
-    ],
-    '/tool/': [
-        {
-            title: '工具箱',
-            collapsable: true,
-            children: [
-                { title: '峰峰软件', path: '/tool/software' },
-                { title: '峰峰作业', path: '/tool/homework' },
-            ]
-        }
+        },
     ]
-    
 }
